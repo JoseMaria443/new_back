6ba20e7d6b2b94c8924e8c5a21c589ac08fc71a3
JoseMaria443 <243752@ids.upchiapas.edu.mx>
2026-07-18 13:16:38 -0600

    bug fix

--- src/modules/personal/application/__init__.py
+++ src/modules/personal/application/__init__.py
@@ -1,12 +1,3 @@
 """
-Use cases del módulo de personal.
+Capa de aplicación del módulo de personal.
 """
-from .login_empleado import LoginEmpleadoUseCase
-from .create_empleado import CreateEmpleadoUseCase
-from .update_empleado_estatus import UpdateEmpleadoEstatusUseCase
-
-__all__ = [
-    "LoginEmpleadoUseCase",
-    "CreateEmpleadoUseCase",
-    "UpdateEmpleadoEstatusUseCase",
-]
